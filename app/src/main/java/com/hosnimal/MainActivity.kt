360e28d192d434f92f5a1f1e9794003bca36df2a
Ubean <ubeannn@gmail.com>
2021-11-03 04:46:46 +0700

    Create Main Fragment

--- app/src/main/java/com/hosnimal/MainActivity.kt
+++ app/src/main/java/com/hosnimal/MainActivity.kt
@@ -2,10 +2,33 @@ package com.hosnimal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.hosnimal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bindingContent:ActivityMainBinding
+    private lateinit var navController:NavController
+    private lateinit var navHostFragment:NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Prepare View Binding
+        bindingContent = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingContent.root)
+
+        // Nav Controller
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.main_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setupSmoothBottomMenu()
+    }
+
+    private fun setupSmoothBottomMenu() {
+        val popupMenu = PopupMenu(this, null)
+        popupMenu.inflate(R.menu.navigation_bar)
+        val menu = popupMenu.menu
+        bindingContent.bottomNavigationBar.setupWithNavController(menu, navController)
     }
 }
