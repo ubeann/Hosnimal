972c291c95fc5288a386f5e70efb93a77934c7b8
Ubean <ubeannn@gmail.com>
2021-12-05 01:00:25 +0700

    Shimmer effect while load image

--- app/src/main/java/com/hosnimal/adapter/ProductAdapter.kt
+++ app/src/main/java/com/hosnimal/adapter/ProductAdapter.kt
@@ -7,6 +7,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.facebook.shimmer.Shimmer
+import com.facebook.shimmer.ShimmerDrawable
 import com.hosnimal.R
 import com.hosnimal.model.Product
 import java.text.NumberFormat
@@ -16,10 +18,23 @@ class ProductAdapter(private val listProduct: List<Product>) : RecyclerView.Adap
     private lateinit var onItemClickCallback: OnItemClickCallback
 
     class ListViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        // Component
         var productImage: ImageView = itemView.findViewById(R.id.image)
         var productName: TextView = itemView.findViewById(R.id.name)
         var productCategory: TextView = itemView.findViewById(R.id.category)
         var productPrice: TextView = itemView.findViewById(R.id.price)
+
+        // Loading
+        private val shimmer = Shimmer.AlphaHighlightBuilder()
+            .setDuration(1800)
+            .setBaseAlpha(0.7f)
+            .setHighlightAlpha(0.6f)
+            .setDirection(Shimmer.Direction.LEFT_TO_RIGHT)
+            .setAutoStart(true)
+            .build()
+        val shimmerDrawable = ShimmerDrawable().apply {
+            setShimmer(shimmer)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder = ListViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.card_product, parent, false))
@@ -27,6 +42,7 @@ class ProductAdapter(private val listProduct: List<Product>) : RecyclerView.Adap
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         Glide.with(holder.productImage.context)
             .load(listProduct[position].images.first())
+            .placeholder(holder.shimmerDrawable)
             .error(R.drawable.ic_round_error_24)
             .into(holder.productImage)
         holder.productName.text = listProduct[position].name
