8e744207c236fc7db7247273c14b08a3d09bb1b9
Ubean <ubeannn@gmail.com>
2021-12-06 20:21:34 +0700

    UI Detail Product

---
+++ app/src/main/java/com/hosnimal/ui/detail_product/DetailProductActivity.kt
@@ -0,0 +1,12 @@
+package com.hosnimal.ui.detail_product
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hosnimal.R
+
+class DetailProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_product)
+    }
+}
