af383260199f1f3bf4cf0f32dbb3f7d47faa2ba1
Ubean <ubeannn@gmail.com>
2021-12-06 23:19:57 +0700

    Transaction Product UI

--- app/src/main/java/com/hosnimal/ui/detail_product/DetailProductActivity.kt
+++ app/src/main/java/com/hosnimal/ui/detail_product/DetailProductActivity.kt
@@ -7,6 +7,8 @@ import android.view.View
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 import com.hosnimal.App
 import com.hosnimal.R
 import com.hosnimal.adapter.ImageSliderAdapter
@@ -26,6 +28,9 @@ class DetailProductActivity : AppCompatActivity() {
 
     // Preferences
     private lateinit var preferences: UserPreferences
+
+    // Data Transaction
+    private var qty = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,9 +59,46 @@ class DetailProductActivity : AppCompatActivity() {
             name.text = product?.name ?: getString(R.string.error_product_not_found)
             category.text = product?.category
             price.text = NumberFormat.getCurrencyInstance(Locale("in", "ID")).format(product?.price).toString().replace(",00","")
+            stock.text = getString(R.string.detail_product_stock, product?.stock)
+            detailOrder.qty.text = qty.toString()
             descriptionText.text = product?.description
         }
 
+        // Setting Button Add
+        product?.let { productData ->
+            binding.detailOrder.btnAdd.setOnClickListener {
+                if (qty == productData.stock) {
+                    showSnackBar(getString(R.string.error_product_buy_maximum))
+                } else {
+                    qty += 1
+                    binding.detailOrder.qty.text = qty.toString()
+                }
+            }
+        }
+
+        // Setting Button Minus
+        binding.detailOrder.btnMinus.setOnClickListener {
+            if (qty == 1) {
+                showSnackBar(getString(R.string.error_product_buy_minimum))
+            } else {
+                qty -= 1
+                binding.detailOrder.qty.text = qty.toString()
+            }
+        }
+
+        // Setting Button Order
+        binding.btnOrder.setOnClickListener {
+            val pricing = NumberFormat.getCurrencyInstance(Locale("in", "ID")).format(product?.price?.times(qty)).toString().replace(",00","")
+            MaterialAlertDialogBuilder(it.context)
+                .setTitle(resources.getString(R.string.detail_product_dialog_title))
+                .setMessage(String.format(getString(R.string.detail_product_dialog_text), qty, product?.name, pricing))
+                .setNegativeButton(resources.getString(R.string.detail_product_dialog_negative)) { _, _ ->
+                }
+                .setPositiveButton(resources.getString(R.string.detail_product_dialog_positive)) { _, _ ->
+                    showSnackBar(String.format(getString(R.string.detail_product_success), qty, product?.name))
+                }
+                .show()
+        }
 
         // Setting Button Back
         binding.btnBack.setOnClickListener {
@@ -67,6 +109,14 @@ class DetailProductActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    private fun showSnackBar(text: String) {
+        Snackbar.make(
+            binding.root,
+            text,
+            Snackbar.LENGTH_SHORT
+        ).show()
     }
 
     companion object {
